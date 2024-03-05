2b8db5bb70031057b6ec29032334792d6076f137
Albert Arrebola Sans <104431726+albertarrebola08@users.noreply.github.com>
2024-03-05 09:28:07 +0100

    Update tasca2.py

--- Classe 3 - API Spotify + Gephy Visualization/tasca2.py
+++ Classe 3 - API Spotify + Gephy Visualization/tasca2.py
@@ -3,24 +3,59 @@
 # 2) Obtenir dades en excel de les dades basiqeus
 # 3) Generar el graph
 
-
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-
+import json
 import pandas as pd
 
 api_client_id = ""
 api_client_secret = ""
 
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(api_client_id,api_client_secret))
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(api_client_id, api_client_secret))
+result_playlist = spotify.playlist_items('37i9dQZF1DX7fvgalTu2lg')
 
-artist_id = '0sKBEhvr6hz7Wpptw0fY8U'
-results = spotify.playlist_items('1vs3kcfG3nJR8tKEILrOya')
+llista_definitiva = []
 
-print(results)
+for e in result_playlist['items']:
+    for artist_data in e['track']['artists']:
+        artist_id = artist_data['id']
+        artist_name = artist_data['name']
+        print(artist_name)
 
+        def get_related(artist_id):
+            resposta = spotify.artist_related_artists(artist_id)
+            return resposta
 
+        result = get_related(artist_id)
 
-df = pd.DataFrame(results, columns = [])
+        llista_de_relacionats = []
 
-df.to_csv("database.csv", sep=",", index=False)
+        for artist in result['artists']:
+            artista = {}
+            artista['origen'] = artist_name
+            artista["desti"] = artist["name"]
+            artista["generes"] = artist["genres"]
+            artista["id"] = artist["id"]
+            llista_de_relacionats.append(artista)
+
+        llista_definitiva.extend(llista_de_relacionats)
+
+llista_tuples = []
+
+for i in llista_definitiva:
+    source = i["origen"]
+    target = i["desti"]
+    tupla = (source, target)
+    llista_tuples.append(tupla)
+
+for i in llista_definitiva:
+    for g in i["generes"]:
+        source = i["desti"]
+        target = g
+        tupla = (source, target)
+        llista_tuples.append(tupla)
+
+df = pd.DataFrame(llista_tuples, columns=["source", "target"])
+
+print(df)
+df.to_csv("graf_playlist.csv", sep=",", index=False)
