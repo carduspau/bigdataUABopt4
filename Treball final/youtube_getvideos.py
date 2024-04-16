1ace12be97970731482b4a68e23cff4f4b979cfb
Albert Arrebola Sans <104431726+albertarrebola08@users.noreply.github.com>
2024-04-16 17:22:08 +0200

    Update youtube_getvideos.py

--- Treball final/youtube_getvideos.py
+++ Treball final/youtube_getvideos.py
@@ -11,6 +11,8 @@ youtube = build(
 
 offset = 0
 token = ""
+
+
 def get_videos(youtube, token, offset):
     request = youtube.search().list(
         part="snippet",
@@ -40,5 +42,35 @@ def get_videos(youtube, token, offset):
             token = response["nextPageToken"]
             get_videos(youtube, token, offset)
 
+
 get_videos(youtube, token, offset)
+
+#Un cop obtinguts els JSONS amb els videos (resum snippet) ara iterem ID per ID i obtenim statistics de cada video
+
+##### GET STATISTICS VIDEOS
+def get_info_videos(youtube, id):
+    request = youtube.videos().list(
+        part="statistics, contentDetails",
+        id=id
+    )
+    response = request.execute()
+
+    with open(f'statistics/{id}.json', 'w', encoding="utf-8") as f:
+        json.dump(response, f)
+
+def read_json(youtube):
+    files = glob.glob("json/*.json")
+    for file in files:
+        with open(file, "r", encoding="utf-8") as f:
+            json_data = json.load(f)
+            videos = json_data["items"]
+            for video in videos:
+
+                idVideo = video["id"]["videoId"]
+                print(idVideo)
+                get_info_videos(youtube, idVideo)
+                break
+            break
+
+read_json(youtube)
 
